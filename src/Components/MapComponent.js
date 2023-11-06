3d0bdab8aa23fb70b3e0ed5c62f7a444e1bf0c9d
ad-klevtsov <ad.klevtsov@gmail.com>
2023-11-06 20:47:14 +0400

    Stops are now toggleable and info pops up on click

--- src/Components/MapComponent.js
+++ src/Components/MapComponent.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import L from 'leaflet';
 import { TileLayer, Marker, Popup, MapContainer } from "react-leaflet";
 import './MapComponent.css';
 import xmlToJson from '../xmlToJson'
 import Stops from "./Stops";
+import './Stops.css';
 
 L.Icon.Default.imagePath = "https://unpkg.com/leaflet@1.5.0/dist/images/";
 
@@ -18,16 +19,37 @@ const stops = await fetch("https://tosamara.ru/api/v2/classifiers/stopsFullDB.xm
 // console.log(parseInt(stops.stop[4].KS_ID['#text']));
 
 function MapComponent(props) {
+
+    const [stopsVisibility, setStopsVisibility] = useState(false);
+
+    const onVisibilityToggle = () => {
+        setStopsVisibility(cur => !cur);
+    }
+
     return (
         <MapContainer zoom={props.zoom} center={[props.lat, props.lng]}>
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+
+            <div className="stops-toggle">
+                <label htmlFor="layertoggle">Показать остановки </label>
+                <input type="checkbox"
+                    name="layertoggle"
+                    id="layertoggle"
+                    value={stopsVisibility}
+                    onChange={onVisibilityToggle}
+                />
+            </div>
+
+            {stopsVisibility &&
+                <Stops stopsList={stops.stop} />
+            }
+
             <Marker position={[props.lat, props.lng]}>
-                <Popup>Тестовый текст</Popup>
+                <Popup>Текущая геопозиция</Popup>
             </Marker>
-            <Stops stopsList={stops.stop} />
         </MapContainer>
     );
 };
--- src/Components/Stops.js
+++ src/Components/Stops.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Marker } from "react-leaflet";
+import { Marker, Popup } from "react-leaflet";
 
 function Stops(props) {
     if (props.stopsList.length > 0) {
         return (<div>
             {props.stopsList.map((el) => (
-                <Marker key={parseInt(el.KS_ID['#text'])} position={[parseFloat(el.latitude['#text']), parseFloat(el.longitude['#text'])]} />
+                <Marker key={parseInt(el.KS_ID['#text'])} position={[parseFloat(el.latitude['#text']), parseFloat(el.longitude['#text'])]}>
+                    <Popup>
+                        <p>{el.title['#text']}</p>
+                        <p>{el.direction['#text']}</p>
+                    </Popup>
+                </Marker>
             ))}
         </div>)
     }
